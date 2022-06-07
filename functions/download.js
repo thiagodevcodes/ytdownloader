facdab4b96b96c439b36a5be3db7a25857659cc0
Thiago Silva <euthiagosrodrigues@gmail.com>
2022-06-07 17:47:28 -0300

    update

---
+++ functions/download.js
@@ -0,0 +1,31 @@
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+
+async function baixarmusica(req, res) {
+    let tipo = req.body.tipo;
+    let link = req.body.link;
+    let info = await ytdl.getInfo(link);
+    let videotitle = info.videoDetails.title;
+    let format;
+    
+    let er = /[^a-z0-9-g()]/gi;
+    videotitle = videotitle.replace(er, " ");
+
+    let download = ytdl(link);
+
+    if(tipo == "MP3") {
+        format = ".mp3";
+        download = ytdl(link,{filter: 'audioonly'});
+    } else {
+        if(tipo == "MP4") {
+            format = ".mp4";
+        }
+    }
+
+    const WriteStream = fs.createWriteStream("./downloads/" + videotitle + format);
+    download.pipe(WriteStream);
+    
+    res.redirect("/");
+}
+
+module.exports = baixarmusica;
--- routes/admin.js
+++ routes/admin.js
@@ -1,38 +1,12 @@
-const ytdl = require("ytdl-core");
-const fs = require("fs");
-const { links } = require("express/lib/response");
 const router = require("express").Router();
+const baixarmusica = require("../functions/download");
 
 router.get( "/", (req, res) => {
-    res.render("home")
+    res.render("home");
 })
 
 router.post( "/baixar", (req, res) => {
-    async function baixarmusica() {
-        let tipo = req.body.tipo;
-        let link = req.body.link;
-        let info = await ytdl.getInfo(link);
-        let videotitle = info.videoDetails.title;
-        let format;
-        
-        let er = /[^a-z0-9-g()]/gi;
-        videotitle = videotitle.replace(er, " ");
-
-        if(tipo == "MP3") {
-            format = ".mp3"
-        } else {
-            if(tipo == "MP4") {
-                format = ".mp4"
-            } else {
-                format = ".mp3"
-            }
-        }
-
-        ytdl(link).pipe(fs.createWriteStream("./downloads/" + videotitle + format));
-        res.redirect("/");
-    }
-
-    baixarmusica()
+    baixarmusica(req, res);
 })
 
 module.exports = router;
